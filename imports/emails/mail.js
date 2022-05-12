dd17b00a61db3fb7e209e131f089df35ac74eb4a
Andrey Grabarnick <Andrey@hyperguest.com>
2022-05-12 18:59:49 +0300

    feat: emails now have a fallback if not enabled

--- imports/emails/mail.js
+++ imports/emails/mail.js
@@ -1,14 +1,16 @@
-import "./templates/enrollAccount/enrollAccount.js";
-import "./templates/resetPassword/resetPassword.js";
+import { Email } from 'meteor/email'
+import './templates/enrollAccount/enrollAccount.js';
+import './templates/resetPassword/resetPassword.js';
 
 Meteor.startup(() => {
   if (Meteor.settings.email.enabled) {
     process.env.MAIL_URL = Meteor.settings.email.smtpUrl;
+  } else {
+    // eslint-disable-next-line no-console
+    Email.send = console.log;
   }
 });
 
-
 // Email Templates
-Accounts.emailTemplates.siteName = "Template";
-Accounts.emailTemplates.from = "Template - noreply <team@template>";
-
+Accounts.emailTemplates.siteName = 'Template';
+Accounts.emailTemplates.from = 'Template - noreply <team@template>';
